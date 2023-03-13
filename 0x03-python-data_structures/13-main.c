b173b88f74bc686cbd53dbb13d5a6c86a0fbf8f3
piustech <barimue.emmanuel@gmail.com>
2023-03-13 05:06:03 -0700

    Task 14

--- 0x03-python-data_structures/13-main.c
+++ 0x03-python-data_structures/13-main.c
@@ -1,33 +1,56 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include "lists.h"
+import ctypes
 
-/**
- * main - check the code for
- *
- * Return: Always 0.
- */
-int main(void)
-{
-	listint_t *head;
-
-	head = NULL;
-	add_nodeint_end(&head, 1);
-	add_nodeint_end(&head, 17);
-	add_nodeint_end(&head, 972);
-	add_nodeint_end(&head, 50);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 98);
-	add_nodeint_end(&head, 50);
-	add_nodeint_end(&head, 972);
-	add_nodeint_end(&head, 17);
-	add_nodeint_end(&head, 1);
-	print_listint(head);
-
-	if (is_palindrome(&head) == 1)
-		printf("Linked list is a palindrome\n");
-	else
-		printf("Linked list is not a palindrome\n");
-	free_listint(head);
-	return (0);
-}
+lib = ctypes.CDLL('./libPyList.so')
+lib.print_python_list_info.argtypes = [ctypes.py_object]
+l = ['hello', 'World']
+lib.print_python_list_info(l)
+del l[1]
+lib.print_python_list_info(l)
+l = l + [4, 5, 6.0, (9, 8), [9, 8, 1024], "My string"]
+lib.print_python_list_info(l)
+l = []
+lib.print_python_list_info(l)
+l.append(0)
+lib.print_python_list_info(l)
+l.append(1)
+l.append(2)
+l.append(3)
+l.append(4)
+lib.print_python_list_info(l)
+l.pop()
+lib.print_python_list_info(l)
+julien@ubuntu:~/CPython$ python3 100-test_lists.py 
+[*] Size of the Python List = 2
+[*] Allocated = 2
+Element 0: str
+Element 1: str
+[*] Size of the Python List = 1
+[*] Allocated = 2
+Element 0: str
+[*] Size of the Python List = 7
+[*] Allocated = 7
+Element 0: str
+Element 1: int
+Element 2: int
+Element 3: float
+Element 4: tuple
+Element 5: list
+Element 6: str
+[*] Size of the Python List = 0
+[*] Allocated = 0
+[*] Size of the Python List = 1
+[*] Allocated = 4
+Element 0: int
+[*] Size of the Python List = 5
+[*] Allocated = 8
+Element 0: int
+Element 1: int
+Element 2: int
+Element 3: int
+Element 4: int
+[*] Size of the Python List = 4
+[*] Allocated = 8
+Element 0: int
+Element 1: int
+Element 2: int
+Element 3: int
