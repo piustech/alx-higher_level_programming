d8d5d3b6afdce6b5674369bde193e487b6769df9
piustech <barimue.emmanuel@gmail.com>
2023-03-28 07:05:50 -0700

    Task 0

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 19 14:21:54 2020
+@author: Robinson Montes
+"""
+
+
+class Square:
+    """Empty class Square that defines a square
+    Attributes:
+        empty
+    """
+    pass
