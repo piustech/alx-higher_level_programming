ddaec2a60ec3b79ff990a5beac29919447bf8d8e
piustech <barimue.emmanuel@gmail.com>
2023-04-03 08:16:13 -0700

    Task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue January 31 2023
+@author: Kris Adelowo
+"""
+
+
+class Rectangle:
+    """Empty class Rectangle that defines a rectangle figure
+    Attributes:
+        empty
+    """
+    pass
